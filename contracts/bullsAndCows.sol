08378bc340e3d24333561c54fac7f5dbd403d6eb
Drygin <DryginAlexander@gmail.com>
2020-07-15 22:30:58 +0300

    add functions

--- contracts/bullsAndCows.sol
+++ contracts/bullsAndCows.sol
@@ -2,10 +2,11 @@ pragma solidity 0.5.11;
 
 
 contract IBullsAndCows {
-    function newGame(uint value, uint digitNumber, string calldata hash) external returns (uint);
+    function newGame(uint value, uint digitNumber, uint attemptNumber, string calldata hash) external returns (uint);
     function deleteGame(uint gameId) external;
     function startGame(uint gameId) external;
     function newGuess(uint gameId, uint[] calldata digits) external;
-    function guessResult(uint gameId, uint bulls, uint cows) external;
+    function guessResult(uint gameId, uint bulls, uint cows, uint[] calldata proof) external;
+    function chalengeResult(uint gameId, uint resultId);
     function forceStopGame(uint gameId) external;
 }
