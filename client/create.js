9d3bd3da89082f18e66498de58972dd75191c735
Sergey Khokhlov <sergeyakhokhlov@gmail.com>
2020-07-19 14:05:49 +0300

    validation

--- client/create.js
+++ client/create.js
@@ -13,13 +13,37 @@ async function load() {
 
     document.getElementById('create-form').addEventListener('submit', (event) => {
         (async () => {
+            let error = false;
             const symbols = new Array(8);
+            let symbolsError = false;
             for (let i = 0; i < 8; i++) {
                 symbols[i] = parseInt(document.getElementById('symbol' + i).value.charCodeAt(0));
+                if (isNaN(symbols[i])) {
+                    symbolsError = true;
+                    error = true;
+                }
+            }
+            if (symbolsError) {
+                document.getElementById('form-error').innerHTML += '<p class="error">All 8 symbols should be present.</p>'
             }
             const guessNumber = parseInt(document.getElementById('guessNumber').value);
+            if (isNaN(guessNumber) || guessNumber <= 0) {
+                error = true;
+                document.getElementById('form-error').innerHTML += '<p class="error">Number of guesses should be above zero.</p>'
+            }
             const bet = document.getElementById('bet').value;
+            if (bet === '' || bet < 0) {
+                error = true;
+                document.getElementById('form-error').innerHTML += '<p class="error">Bet value should be greater than or equal to zero.</p>'
+            }
             const nonce = document.getElementById('nonce').value;
+            if (nonce === '') {
+                error = true;
+                document.getElementById('form-error').innerHTML += '<p class="error">Nonce should be present.</p>'
+            }
+            if (error) {
+                return;
+            }
             // let input = {
             //     "zero": 0,
             //     "digits": symbols,
@@ -41,6 +65,7 @@ async function load() {
             try {
                 await myContract.methods.newGame(weiValue, 8, guessNumber, window.signalHash(symbols, nonce))
                     .send({from: accounts[0], value: weiValue});
+                document.getElementById('create').innerHTML += `<p>The game has been created.</p>`;
             } catch (e) {
                 document.getElementById('create').innerHTML += `<p class="error">${e.message}</p>`;
                 throw new Error(e);
