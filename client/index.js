dae1d51d0027439cce3d871f6ed58ffcce7a77f0
Sergey Khokhlov <sergeyakhokhlov@gmail.com>
2020-07-19 16:56:16 +0300

    final list

--- client/index.js
+++ client/index.js
@@ -12,6 +12,7 @@ async function load() {
     document.getElementById('loading').innerText = '';
 
     const games = document.getElementById('games');
+    const gamesFinalized = document.getElementById('finalized-games');
     const gamesCount = parseInt(await myContract.methods.getGamesCount().call());
     for (let i = 0; i < gamesCount; i++) {
         const game = await myContract.methods.games(i).call();
@@ -21,44 +22,9 @@ async function load() {
         } else if (game.status == 1) {
             status = 'Started';
         } else {
+            gamesFinalized.innerHTML += `<tr><td>#${i}</td><td>${game.guessNumber}</td><td>${game.guessCounter}</td><td>${web3.utils.fromWei(game.value, 'ether')} ETH</td><td>${game.host}</td><td>Finished</td><td><a href="game.html?gameId=${i}">Open Game</a></td></tr>`;
             continue;
         }
         games.innerHTML += `<tr><td>#${i}</td><td>${game.guessNumber}</td><td>${game.guessCounter}</td><td>${web3.utils.fromWei(game.value, 'ether')} ETH</td><td>${game.host}</td><td>${status}</td><td><a href="game.html?gameId=${i}">Open Game</a></td></tr>`;
     }
-}
-
-async function verifyProofByIndex(address, index) {
-    let periodProof = await myContract.methods.periodProofs(address, index).call();
-    let a = await myContract.methods.signals(address, 2 * index).call(); //TODO change function
-    let b = await myContract.methods.signals(address, 2 * index + 1).call();
-    let price_a = Math.round((await oracle.methods.currentAnswer().call(a.blockNumber)) / Math.pow(10, 8));
-    let price_b = Math.round((await oracle.methods.currentAnswer().call(b.blockNumber)) / Math.pow(10, 8));
-    // let currentBlock = await web3.eth.getBlockNumber();
-    let price_now = Math.round((await oracle.methods.currentAnswer().call(periodProof.blockNumber)) / Math.pow(10, 8));
-    // let price_a = 100;
-    // let price_b = 200;
-    // let price_now = 300;
-    let previousBalanceHash;
-    if (index === 0) {
-        previousBalanceHash = '12991363837217894993991711342410433599666196004667524206273513024950584067662';
-    } else {
-        previousBalanceHash = (await myContract.methods.periodProofs(address, index - 1).call()).newBalanceHash;
-    }
-    let parameters = [
-        periodProof.newBalanceHash, periodProof.y, previousBalanceHash, a.hash, b.hash, price_a, price_b, price_now
-    ];
-    verify(window.verificationKey, parameters, periodProof.proof, document.getElementById(`verify-${address}-${index}`));
-}
-
-function verify(verificationKey, publicSignals, proof, block) {
-    return new Promise((resolve) => {
-        const start = new Date().getTime();
-        block.innerHTML = "processing....";
-        window.groth16Verify(verificationKey, publicSignals, proof).then((res) => {
-            const end = new Date().getTime();
-            const time = end - start;
-            // document.getElementById("time").innerHTML = `Time to compute: ${time}ms`;
-            block.innerHTML = (res === true) ? '<span class="good">GOOD</span>' : '<span class="failed">Failed</span>';
-        });
-    });
 }
